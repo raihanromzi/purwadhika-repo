281e5335830b4525b5f85b9efb65b9fa6dbc5295
Raihan Romzi <raihan.rakhman16@gmail.com>
2022-11-16 16:10:01 +0700

    day7

--- 2. Class Code/Day 7/exercise.js
+++ 2. Class Code/Day 7/exercise.js
@@ -1,3 +1,4 @@
+// 1
 const isObject = (object) => {
   return object != null && typeof object === 'object'
 }
@@ -19,6 +20,7 @@ const isEqual = (obj1, obj2) => {
   return true
 }
 
+// 2
 const obj1 = {
   a: '1',
   b: '2',
@@ -29,11 +31,39 @@ const obj2 = {
   a: '1',
   c: '3'
 }
-
-for (const key1 of Object.keys(obj1)) {
-  for (const key2 of Object.keys((obj2))) {
-    if (key1 === key2) {
-      console.log(`{${key1}: ${obj1[key1]}}`)
+const getIntersection = (obj1, obj2) => {
+  for (const key1 of Object.keys(obj1)) {
+    for (const key2 of Object.keys((obj2))) {
+      if (key1 === key2) {
+        console.log(`{${key1}: ${obj1[key1]}}`)
+      }
     }
   }
 }
+
+// 3
+const array1 = [
+  { name: 'Student 1', email: 'student1@mail.com' },
+  { name: 'Student 2', email: 'student2@mail.com' }
+]
+const array2 = [
+  { name: 'Student 1', email: 'student1@mail.com' },
+  { name: 'Student 3', email: 'student3@mail.com' },
+]
+
+const mergeTwoArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr2.length; i++) {
+    for (let j = 0; j < arr1.length; j++) {
+      if ((arr2[i].name === arr1[j].name) && (arr2[i].email === arr1[j].email)) {
+        i++
+        j++
+      }
+      if (arr2[i]) {
+        arr1.push(arr2[i])
+      }
+    }
+  }
+  return arr1
+}
+
+console.log(mergeTwoArrays(array1, array2))
